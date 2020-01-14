fb40cc677ae9e982ddc1336a09b01f548c20d330
Montse Caballero <monoju_montse15@hotmail.com>
2020-01-14 12:59:15 -0600

    Animación en Table View

---
+++ TableCellAnimation/TableCellAnimation/ArticleTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  ArticleTableViewCell.swift
+//  TableCellAnimation
+//
+//  Created by Simon Ng on 3/10/2016.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    @IBOutlet weak var titleLabel:UILabel!
+    @IBOutlet weak var postImageView:UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TableCellAnimation/TableCellAnimation/ArticleTableViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ArticleTableViewController.swift
+//  TableCellAnimation
+//
+//  Created by Simon Ng on 3/10/2016.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class ArticleTableViewController: UITableViewController {
+    
+    let postTitles = ["Use Background Transfer Service To Download File in Background",
+                      "Face Detection in iOS Using Core Image",
+                      "Building a Speech-to-Text App Using Speech Framework in iOS 10",
+                      "Building Your First Web App in Swift Using Vapor",
+                      "Creating Gradient Colors Using CAGradientLayer",
+                      "A Beginner's Guide to CALayer"];
+    let postImages = ["imessage-sticker-pack", "face-detection-featured", "speech-kit-featured", "vapor-web-framework", "cagradientlayer-demo", "calayer-featured"];
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.estimatedRowHeight = 258.0
+        tableView.rowHeight = UITableViewAutomaticDimension
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .automatic
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return postTitles.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! ArticleTableViewCell
+        
+        // Configure the cell...
+        cell.titleLabel.text = postTitles[(indexPath as NSIndexPath).row]
+        cell.postImageView.image = UIImage(named: postImages[(indexPath as NSIndexPath).row])
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        /*
+        //Define the initial state (Before the animation)
+        cell.alpha = 0
+        
+        //Define the final state (After the animation)
+        UIView.animate(withDuration: 1.0, animations: { cell.alpha = 1.0})
+        */
+        
+        //Define the initial state (Before the animation)
+        //let rotationAngleInRadians = 90.0 * CGFloat(Double.pi / 180.0)
+        //let rotationTransform = CATransform3DMakeRotation(rotationAngleInRadians, 0, 0, 1)
+        let rotationTransform = CATransform3DTranslate(CATransform3DIdentity, -500, 100, 0)
+        
+        cell.layer.transform = rotationTransform
+        
+        //Define the final state (After the animation)
+        UIView.animate(withDuration: 1.0, animations: { cell.layer.transform = CATransform3DIdentity})
+    }
+}
