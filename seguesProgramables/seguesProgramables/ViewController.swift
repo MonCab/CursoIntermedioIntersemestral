23c319df047a887efb442157a723f5be35d8f74f
Hugo_Cardi <hugo.cardi@gmail.com>
2020-01-15 03:43:34 -0600

    Segues

---
+++ seguesProgramables/seguesProgramables/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  seguesProgramables
+//
+//  Created by Hugo Cruz on 9/18/19.
+//  Copyright © 2019 Hugo Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+	
+	@IBAction func unwindtToRed(unwindSegue: UIStoryboardSegue){
+		
+	}
+
+}
+
